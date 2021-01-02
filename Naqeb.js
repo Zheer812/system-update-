f00782d69e07ed63b94eba23a26a76bdb599b974
drzheercid <76868255+drzheercid@users.noreply.github.com>
2021-01-02 21:40:58 +0300

    Update Naqeb.js

--- Naqeb.js
+++ Naqeb.js
@@ -25,7 +25,7 @@ client.login("Nzc5NjUwMjgwOTc1MzY4MTky.X7jn6A.ryWD8MsNbKoSlekUeZskoORZZnA");
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setStatus("idle");
-  client.user.setActivity(`${prefix}help ,Users ${client.users.cache.size} `, { type: "PLAYING" });
+  client.user.setActivity(`${prefix}help`, { type: "PLAYING" });
   client.guilds.cache.forEach(g => {
     if (g.member(client.user).hasPermission("ADMINISTRATOR")) {
       g.fetchInvites().then(guildInvites => {});
