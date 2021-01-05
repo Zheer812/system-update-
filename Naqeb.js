963ebb03aa4a34c34e1bf73e2f5640c0af341c82
drzheercid <76868255+drzheercid@users.noreply.github.com>
2021-01-05 21:28:28 +0300

    Update Naqeb.js

--- Naqeb.js
+++ Naqeb.js
@@ -21,7 +21,7 @@ var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
 const prefix = "-";
-client.login("Nzc5NjUwMjgwOTc1MzY4MTky.X7jn6A.ryWD8MsNbKoSlekUeZskoORZZnA");
+client.login("");
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setStatus("idle");
